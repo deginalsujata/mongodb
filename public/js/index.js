68bfbdd54b4a0a95e4acdf4b797d2c8a20688830
sujatha <sujatha199105@gmail.com>
2016-09-01 15:10:44 -0700

    no message
    Signed-off-by: sujatha <sujatha199105@gmail.com>

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+﻿var socket = io();
+$('#send-message-btn').click(function () {
+    var msg = $('#message-box').val();
+    var uname = $('#user1').val();
+    socket.emit('chat', msg);
+    socket.emit('chat', uname);
+    $('#messages').append($('<p>').text(msg));
+    $('#message-box').val('');
+    $('#user1').val('');
+    return false;
+});
+socket.on('chat', function (msg) {
+    $('#messages').append($('<p>').text(msg));
+});
